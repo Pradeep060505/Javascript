20a2cc03ac69c61f992919cfd43460c9e2ba9a97
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-18 17:49:53 +0000

    summary of datatype-1

---
+++ Basics/datatype_summary.js
@@ -0,0 +1,18 @@
+//Primitive
+//7 types : string, Number, Boolean, null, undefined, Symbol, BigInt
+
+// we don't define javascript
+const score = 100
+
+const isLoggedIn = false
+let userEmail; //undefined
+
+//symbol is used for uniqueness
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+//Reference(Non primitive) 
+
+//Array, Objects, Functions
