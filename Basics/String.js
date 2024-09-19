74ae4cd038ba4cbe06c78c806de3b4f41c079021
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-19 18:05:26 +0000

    string syntax

---
+++ Basics/String.js
@@ -0,0 +1,9 @@
+const name  = "Pradeep"
+const repoCount = 15
+
+//console.log(name + repoCount + "value");
+
+//good way
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const firstname = new String("Pradeep")
