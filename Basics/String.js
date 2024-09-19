92d9d2be5bd59f861fadfd06191eed6f6e26012e
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-19 18:14:24 +0000

    done with some string function

--- Basics/String.js
+++ Basics/String.js
@@ -7,3 +7,14 @@ const repoCount = 15
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
 const firstname = new String("Pradeep")
+
+//accesing keyvalue 
+console.log(firstname[0])
+
+//access prototype
+console.log(firstname.__proto__)
+
+console.log(firstname.length)
+console.log(firstname.toUpperCase())
+console.log(firstname.charAt(3))
+console.log(firstname.indexOf('a'))
