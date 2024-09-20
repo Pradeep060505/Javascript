a8fd93e9a38939984881d63171b03ae80f2465f5
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-20 16:31:37 +0000

    done with date in js

---
+++ Basics/date.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+
+let mynewDate = new Date(2024, 1, 25)
+console.log(mynewDate.toLocaleDateString());
