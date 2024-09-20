7bfdfaf9df8e3e66560e6d183225c6197e929b0e
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-20 16:19:46 +0000

    number and math done

--- Basics/number_and_maths.js
+++ Basics/number_and_maths.js
@@ -15,3 +15,20 @@ console.log(otherNumber.toPrecision(3))
 const hundreds = 100000
 console.log(hundreds.toLocaleString('en-IN'));
 
+//*****maths*****//
+
+console.log(Math)
+//absolute means only negative value converted into positive
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2))//if value greater than given number will roundoff to next number ex.4.2>5
+console.log(Math.floor(4.6))//floor choosed lower value than given 4.6 > 4
+
+console.log(Math.random())//value will come between 0 to 1
+console.log(Math.random()*10 + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1)))
