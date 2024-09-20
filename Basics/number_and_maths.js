9ef025f52d6531bbc65e3327bb737626449e57dc
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-20 16:05:52 +0000

    started with numbers

---
+++ Basics/number_and_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score)
+
+//defining number exclusively
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))// for precision value 100.00
+
+//precison value
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
