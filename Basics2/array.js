c4e02f2c612b288089ef339defa39b69cd977e82
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-20 16:56:14 +0000

    learn about array declaration and push pop unshift and shift method

---
+++ Basics2/array.js
@@ -0,0 +1,18 @@
+//array
+
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const heros = ["shaktiman", "iron-man"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+//array methods
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9);//to add in start but not optimize
+myArr.shift()//to remove unshift added value
+
+console.log(myArr)
