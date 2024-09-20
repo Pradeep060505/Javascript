9ecb1726c000c09241ed22c0f2ce291e824f8251
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-09-20 16:41:03 +0000

    basics of date

--- Basics/date.js
+++ Basics/date.js
@@ -8,3 +8,21 @@ console.log(myDate.toLocaleDateString());
 
 let mynewDate = new Date(2024, 1, 25)
 console.log(mynewDate.toLocaleDateString());
+
+//timestamp
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+//to simplify
+
+console.log(myTimeStamp.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+//methods
+
+let newDate2 = new Date()
+
+console.log(newDate2)
+console.log(newDate2.getDay())
+
